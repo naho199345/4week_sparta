a1eb76994148573acd247902807dd4ff36492b7e
김승호 <naho199345@naver.com>
2022-02-01 22:17:00 +0900

    몽구스 주소 변경

--- schemas/index.js
+++ schemas/index.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-const connect = () => {
-    mongoose.connect('mongodb://test:test@3.36.71.17:27017/admin', { ignoreUndefined:true }).catch((err) => {
-        console.error(err);
-    })
-};
-
-// // ec2
 // const connect = () => {
-//     mongoose.connect('mongodb://localhost:27017/spa_article', { ignoreUndefined:true }).catch((err) => {
+//     mongoose.connect('mongodb://test:test@3.36.71.17:27017/admin', { ignoreUndefined:true }).catch((err) => {
 //         console.error(err);
 //     })
 // };
+
+// // ec2
+const connect = () => {
+    mongoose.connect('mongodb://localhost:27017/spa_article', { ignoreUndefined:true }).catch((err) => {
+        console.error(err);
+    })
+};
 
 module.exports = connect;
